552a8d144ffa1f47fcf379d838632a21dbadfcb3
SuneTheFrack <sunefrack69@gmail.com>
2025-08-03 14:50:54 +0200

    Update sheets.py

--- utils/sheets.py
+++ utils/sheets.py
@@ -1,12 +1,13 @@
 import gspread
 import os
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
+# == ge rättigheter att läsa och skriva ==
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+# == skriv till sheet ==
 def skriv_till_sheet(rad, blad_namn="Logg"):
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive"
-    ]
     
     import json  # lägg detta högst upp om det inte finns
     creds_dict = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
@@ -19,3 +20,21 @@ def skriv_till_sheet(rad, blad_namn="Logg"):
     blad = spreadsheet.worksheet(blad_namn)
     blad.append_row(rad)
 
+# == läs från sheet ==
+def hamta_loggar(blad_namn="Logg", person=None, datum=None):
+    creds_dict = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
+    spreadsheet = client.open_by_key(os.environ["SHEET_ID"])
+    blad = spreadsheet.worksheet(blad_namn)
+    rader = blad.get_all_records()
+
+    filtrerade = []
+    for rad in rader:
+        if person and rad["person"].lower() != person.lower():
+            continue
+        if datum and rad["datum"] != datum:
+            continue
+        filtrerade.append(rad)
+
+    return filtrerade

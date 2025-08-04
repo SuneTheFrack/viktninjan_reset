4e91d0dea2af9ce32bf213511729835a1daf7a74
SuneTheFrack <sunefrack69@gmail.com>
2025-08-04 20:26:32 +0200

    Update tidutils.py

--- tidutils.py
+++ tidutils.py
@@ -1,20 +1,13 @@
-# utils.py
-# Hjälpfunktioner för loggmoduler
-
 from datetime import datetime
-from zoneinfo import ZoneInfo
-
 
 def get_datum_tid(data):
-    now = datetime.now(ZoneInfo("Europe/Stockholm"))
-
     datum = data.get("datum")
-    if not datum or str(datum).strip().lower() == "nu":
-        datum = now.strftime("%Y-%m-%d")
-
     tid = data.get("tid")
-    if not tid or str(tid).strip().lower() == "nu":
-        tid = now.strftime("%H:%M")
+
+    if not datum or datum == "nu":
+        datum = datetime.now().strftime("%Y-%m-%d")
+
+    if not tid or tid == "nu":
+        tid = datetime.now().strftime("%H:%M")
 
     return datum, tid
-
